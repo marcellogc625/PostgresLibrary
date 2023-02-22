6de3156de05ca44a904724c669b7b33036b087dd
Marcello Galvão <marcellogc25@gmail.com>
2023-02-22 10:44:51 -0300

    Utils object updated.

--- src/main/java/postgreslibrary/gui/utils/Utils.java
+++ src/main/java/postgreslibrary/gui/utils/Utils.java
@@ -1,9 +1,17 @@
 package postgreslibrary.gui.utils;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.Locale;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.stage.Stage;
 
 public class Utils {
     // Tries to convert a String value to an Integer value //
@@ -42,6 +50,22 @@ public class Utils {
             };
             return cell;
         });
+    }
+    // Load external window method //
+    public static void loadExternalWindow(String path, String titleWindow){
+        try {
+            Stage stage = new Stage();
+            URL url = Paths.get(path).toUri().toURL();
+            Parent root = FXMLLoader.load(url);
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.setTitle(titleWindow);
+            stage.show();
+        }
+        catch(IOException e){
+            Alerts.showAlert(null, "Error in opening window:", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 
 }
